3dd045615f910ed90655bc1be85be7fd2ce7c8b6
yudianwebdev <69705828+yudianwebdev@users.noreply.github.com>
2022-07-06 11:57:32 +0700

    fix pwa (#8)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+const WithPWA = require("next-pwa");
 const nextConfig = {
   reactStrictMode: true,
   i18n: {
@@ -6,5 +7,14 @@ const nextConfig = {
     defaultLocale: "en",
   },
 };
+// withOffline(nextConfig);
 const withOffline = require("next-offline");
-module.exports = withOffline(nextConfig);
+module.exports = WithPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
---
+++ pages/_document.js
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" type="image" href="/favicon.png"></link>
+          <link
+            rel="manifest"
+            href="/manifest.json"
+            crossOrigin="use-credentials"
+          />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
