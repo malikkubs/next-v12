0a7597aaac6779cc625b3d906a2d29ab0c0847da
yudian nanda <yudiannanwebdev@gmail.com>
2023-11-14 09:19:22 +0700

    next-config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
-const WithPWA = require("next-pwa");
 const nextConfig = {
-  // reactStrictMode: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  /* config options here */
 };
-// withOffline(nextConfig);
-const withOffline = require("next-offline");
-module.exports = WithPWA({
-  // reactStrictMode: true,
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  },
-});
+
+module.exports = nextConfig;
