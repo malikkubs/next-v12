d598759ada30850c6185c4751e48be21552b359f
Yudian <yudiannanwebdev@gmail.com>
2023-12-05 16:40:16 +0700

    ganti tag v1.0.1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ function HomePage() {
   return (
     <Layout>
       <div className="bg-red-500">
-      <>jalan</>
+      <>jalan ini ganti tagv1.0.1</>
         test<div>yudian</div>
         test<div>yudian</div>
       </div>
